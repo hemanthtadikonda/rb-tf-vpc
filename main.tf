c0791ead594807a55b09ac0050006affc8958ecd
hemanth <hemanthtadikonda29@gmail.com>
2023-11-16 19:52:45 +0530

    pc -vpc

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
 resource "aws_vpc" "main" {
-  cidr_block = var.vpc_id
+  cidr_block = var.cidr
+}
+
+module "subnets" {
+  source = "./subnets"
+  for_each = var.subnets
+  vpc_id = aws_vpc.main.id
+  subnets = each.value
+
+
 }
---
+++ subnets/main.tf
@@ -0,0 +1,24 @@
+resource "aws_subnet" "main" {
+  for_each           = var.subnets
+
+  vpc_id             = var.vpc_id
+  cidr_block         = each.value["cidr"]
+  availability_zone  = each.value["az"]
+
+  tags = {
+    Name = each.key
+  }
+}
+resource "aws_route_table" "main" {
+  for_each = var.subnets
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "${each.key}-rt"
+  }
+}
+resource "aws_route_table_association" "a" {
+  for_each = var.subnets
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.main.id
+}
