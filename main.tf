e2d873215f0825500185f96b858fc3665a125d78
hemanth <hemanthtadikonda29@gmail.com>
2023-11-16 20:18:32 +0530

    pc -vpc

--- main.tf
+++ main.tf
@@ -8,5 +8,17 @@ module "subnets" {
   vpc_id = aws_vpc.main.id
   subnets = each.value
 
-
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "vpc-igw"
+  }
+}
+#resource "aws_route" "r" {
+#  route_table_id            = lookup(lookup(module.subnets, "public",null )
+#  destination_cidr_block    = "0.0.0.0/0"
+#  gateway_id                = aws_internet_gateway.igw.id
+#}
