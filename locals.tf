7d9463c2ecb878ba311fdf85a6d6a9534c22ee4f
hemanth <hemanthtadikonda29@gmail.com>
2023-11-17 16:52:33 +0530

    pc -vpc

--- locals.tf
+++ locals.tf
@@ -6,6 +6,10 @@ locals {
   private_subnet_id = concat(local.app_subnets_id , local.db_subnets_id)
 
   # RT
+  public_route_table_ids = [for k,v in lookup(lookup(module.subnets,"public",null ),"route_table_ids",null) : v.id ]
+  app_route_table_ids = [for k,v in lookup(lookup(module.subnets,"app",null ),"route_table_ids",null) : v.id ]
+  db_route_table_ids = [for k,v in lookup(lookup(module.subnets,"db",null ),"route_table_ids",null) : v.id ]
+  private_route_table_ids = concat(local.app_route_table_ids , local.db_route_table_ids)
 
 
 }
